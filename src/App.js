f160c9c85a3f8ebb0bcb254c8a5551df5617aa24
alex-samari <alexsamarim@gmail.com>
2019-01-08 10:30:09 -0500

    Added a deleteToDo function

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ class App extends Component {
   state = {
     todos: [
       { id: 1, content: 'buy some milk' },
-      { id: 2, content: 'play some games' }
+      { id: 2, content: 'finish react project' }
     ]
+  }
+
+  deleteToDo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      //  since the key and the value both have the same name we can use the following syntax instead of "todos: todos"
+      todos
+    })
   }
   render() {
     return (
       <div className="todo-app  container">
         <h1 className="center blue-text">Todos</h1>
-        <Todos todos={this.state.todos} />
+        {/* component todos with props "todos" and deleteToDo*/}
+        {/* passing props in here gives us access to the props in the corresponding component */}
+        <Todos todos={this.state.todos} deleteToDo = {this.deleteToDo}/>
       </div>
     );
   }
--- src/Todos.js
+++ src/Todos.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 //  creating a functional Todo component
-const Todos = ({todos}) => {
-    
+const Todos = ({todos, deleteToDo}) => {
+    //  check to see if there are any todos available
     const toDoList = todos.length ? (
-        todos.map(todo => {
+        //  cycling through the todos array and copying the values into a new array
+        todos.map((todo) => {
             return (
+                //  colletion-item class is a materialize.css class
                 <div className="collection-item" key={todo.id}>
-                    <span>{todo.content}</span>
+                    {/* Wrapping the deleteToDo function in an anonymous function is necessary otherwise
+                    the deleteToDo fires up everytime a toDoList is created */}
+                    <span onClick={() => {deleteToDo(todo.id)}}>{todo.content}</span>
                 </div>
             )
         })
